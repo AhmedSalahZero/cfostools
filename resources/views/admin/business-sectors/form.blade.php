9bd6714bf85d6abda4e2986f4914102fef93a22a
AhmedSalahZero <55031551+AhmedSalahZero@users.noreply.github.com>
2024-01-03 10:15:30 +0200

    change path of yml file

--- resources/views/admin/business-sectors/form.blade.php
+++ resources/views/admin/business-sectors/form.blade.php
@@ -1,4 +1,6 @@
-        <form  class="kt-form kt-form--label-right" method="{{ $method ?? 'POST' }}"  enctype="multipart/form-data"  action="{{  isset($model) ? $updateRoute : $storeRoute  }}" >
+        {{-- comment here --}}
+		
+		<form  class="kt-form kt-form--label-right" method="{{ $method ?? 'POST' }}"  enctype="multipart/form-data"  action="{{  isset($model) ? $updateRoute : $storeRoute  }}" >
         @csrf 
         <input type="hidden" name="current_business_sector_id" id="current_business_sector_id" value="" data-value="0">
         <label class="form-label">{{ __('Business Sector Name') }}</label>
